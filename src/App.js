a2af58a6e70596befe955ba653b7aa05f4a4d815
rajesh pandey <rajeshpandry24@gmail.com>
2022-12-03 16:30:39 +0545

    ready to lunch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import Edit from './components/Edit';
-// import Home from './components/Home';
-
-
+import React from "react";
+// import Edit from './components/Edit';
+import Home from "./components/Home";
 
 function App() {
   return (
-   
     <>
-          {/* <Home/>  */}
-          <Edit/>
+      <Home />
+      {/* <Edit/> */}
     </>
   );
 }
